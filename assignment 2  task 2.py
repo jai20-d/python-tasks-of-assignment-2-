5cf34cb8d3c8f0c38e515ca8ef2cac9a765856e8
jai20-d <jaitomarjai20@gmail.com>
2025-10-01 16:55:54 +0530

    Add files via upload

---
+++ assignment 2  task 2.py
@@ -0,0 +1,5 @@
+counter=0
+for i in range(1,51) :
+    counter=counter+i
+
+print('the sum of number 1 to 50 is:',counter)
---
+++ assignment 2 task 1.py
@@ -0,0 +1,7 @@
+#checking odd even number
+
+num=int(input('enter a number: '))
+if (num//2)*2 == num :
+    print(num, 'is even')
+else:
+    print(num, 'is odd')
